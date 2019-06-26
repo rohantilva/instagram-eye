fdb5f6a49068df0ca0c7bb13c34271e51c775370
rohantilva <rohan.tilva@gmail.com>
2019-06-26 18:42:12 -0400

    find verified followers

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
 from InstagramAPI import InstagramAPI
-import sys
+import sys, pprint
 
 username, password, option = sys.argv[1], sys.argv[2], sys.argv[3]
 instaObject = InstagramAPI(username, password)
 
 def findBadFriends():
-    followers = []
-    following = []
-    for person in instaObject.getTotalSelfFollowers():
-        followers.append(person.get("username"))
-    for person in instaObject.getTotalSelfFollowings():
-        following.append(person.get("username"))
-
+    followers = getFollowers()
+    following = getFollowing()
     badFriends = set(following).difference(set(followers))
     print(badFriends)  
 
+def getFollowers():
+    followers = []
+    for person in instaObject.getTotalSelfFollowers():
+        followers.append(person.get("username"))
+    return followers
+
+def getFollowing():
+    following = []
+    for person in instaObject.getTotalSelfFollowings():
+        following.append(person.get("username"))
+    return following
+
+def getVerifiedFollowing():
+    verified = []
+    for person in instaObject.getTotalSelfFollowings():
+        if person.get("is_verified"):
+            verified.append(person.get("username"))
+    return verified 
+    
 def main():
     instaObject.login()
-    findBadFriends()
+
+
 
 if __name__ == "__main__":
     main()
